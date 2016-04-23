8ef75a2a966ad30d70106eebce63c021536fb15b
eoglethorpe <ewanogle@gmail.com>
2016-04-23 22:40:34 +0545

    function qrgen

--- utils/qrgen.py
+++ utils/qrgen.py
@@ -3,13 +3,18 @@ create a user definimed number of unique QR codes and export to CSV
 """
 
 import csv
+import os
 
 import click
 import qrcode
 import qrcode.image.svg
+import time
 from random import randint
 
 def ingest(path, col_name):
+    """
+    read in existing IDs
+    """
     f = open(path, 'rt')
     ids = []
 
@@ -25,22 +30,21 @@ def ingest(path, col_name):
 
     return ids
 
-
-def save(img):
-    print img
-    with open('/Users/ewanog/code/repos/hfh/utils/' + str(img),'w+') as imgfile:
-        img.save(imgfile,'SVG')
-
 def genids(baseids, num):
     """
     return a array with a tuple of associated codes and qr images
+
+    dict contains:
+    id | int
+    code | QR object
+    path | str (initial null)
     """
     factory = qrcode.image.svg.SvgPathImage
     imgs = []
-#    minr = 100000
-#    maxr = 999999
-    minr = 1
-    maxr = 10
+    minr = 100000
+    maxr = 999999
+#    minr = 1
+#    maxr = 10
     print baseids
 
     for i in xrange(num):
@@ -49,19 +53,46 @@ def genids(baseids, num):
             print str(newid) + 'is in!'
             newid = randint(minr,maxr)
 
-        newcodeloc = save(qrcode.make(newid, image_factory=factory))
-        imgs += [(newid, newcodeloc)]
+        newcode = qrcode.make(newid, image_factory=factory)
         baseids += [newid]
+        imgs += [{'id' : newid, 'code' : newcode, 'path' : None}]
 
     if len(baseids) != len(set(baseids)):
-        raise Exception('There are duplicates! Halting...')
+        raise Exception('There are duplicates! Freak out! Halting...')
 
     return imgs
 
+def saveimg(idimg, path):
+    """
+    save all teh images
+    """
+    path =  _get_path(path)
 
-def export(idimg, path):
-    pass
+    if not os.path.exists(path):
+        os.makedirs(path)
 
+    for v in idimg:
+        #v[0] = name, v[1] = image
+        v['path'] = path
+        with open(path + str(v['id']) + '.svg', 'w+') as imgfile:
+            v['code'].save(imgfile,'SVG')
+
+
+def createcsv(idimg, path):
+    """
+    create a CSV with id and path to images
+    """
+    f = open(_get_path(path) + 'qrout.csv', 'wt')
+    try:
+        writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(["id","@qr"])
+        for v in idimg:
+            writer.writerow([v['id'],v['path']])
+    finally:
+        f.close()
+
+def _get_path(path):
+    return os.path.dirname(path) + '/imgs/' + time.strftime("%Y-%m-%d-%H%M") + '/'
 
 @click.command()
 @click.option('--path', help='path to CSV')
@@ -70,7 +101,8 @@ def export(idimg, path):
 def run(path, col_name, num):
         baseids = ingest(path, col_name)
         idimg = genids(baseids, num)
-        export(idimg, path)
+        saveimg(idimg, path)
+        createcsv(idimg, path)
 
 if __name__ == '__main__':
     run()
